b11a7e338d3e90912754ece8f3237203aa4e1d33
KondoSuguru <suguru.pkmn@gmail.com>
2019-06-15 14:16:09 +0900

    Player追加、画面サイズの変更

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal") * speed;
+        rb.AddForce(x, 0, 0);
+    }
+}
