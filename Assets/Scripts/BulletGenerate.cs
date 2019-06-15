101b6a3f1f1d3d147e132b0dcb79eb57cc8c625c
TakanoKakeru <llmn.0419@gmail.com>
2019-06-15 15:57:33 +0900

    弾をスペースで発射可能

---
+++ Assets/Scripts/BulletGenerate.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletGenerate : MonoBehaviour {
+    public GameObject bullet;
+    public float shotTime = 1f;
+    float time = 0f;
+    bool canShot = false;
+
+    // Start is called before the first frame update
+    //IEnumerator Start() {
+    //    while (true) {
+    //        if (Input.GetKey(KeyCode.Space) && canShot) {
+    //            // 弾をプレイヤーと同じ位置/角度で作成
+    //            Instantiate(bullet, transform.position, transform.rotation);
+    //            canShot = false;
+    //        }
+    //        // 1秒待つ
+    //        yield return new WaitForSeconds(1f);
+    //    }
+    //}
+
+    private void Update() {
+        if (Input.GetKey(KeyCode.Space) && canShot) {
+            // 弾をプレイヤーと同じ位置/角度で作成
+            Instantiate(bullet, transform.position, transform.rotation);
+            canShot = false;
+        }
+
+        CanShot();
+    }
+
+    void CanShot() {
+        time++;
+        //canShot = time >= 60 * shotTime ? true : false;
+        if (time >= 60 * shotTime) {
+            canShot = true;
+            time = 0f;
+        }
+    }
+}
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+    public float speed = 0.01f;
+    Rigidbody rb;
+
+    private void Update() {
+        //rb.AddForce(speed, 0, 0);
+        //rb.velocity += new Vector3(speed, 0, 0);
+        transform.position += new Vector3(0, 0, speed);
+    }
+}
