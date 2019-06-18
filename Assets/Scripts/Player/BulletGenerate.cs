f9b13e9d0c0ffdec95eae155276493169e828cea
TakanoKakeru <llmn.0419@gmail.com>
2019-06-18 14:59:37 +0900

    オプションを追加(x)

--- Assets/Scripts/Player/BulletGenerate.cs
+++ Assets/Scripts/Player/BulletGenerate.cs
@@ -1,22 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletGenerate : MonoBehaviour {
     public float shotTime = 0.1f;
+    public float optionPos = 4f;
     public float optionSpeed = 0.3f;
-    public GameObject bullet;
+    public float optionTrackingSpeed = 0.05f;
+    public Transform bullet;
     public GameObject option;
     GameObject[] options;
+    Vector3[] optionDefaultPos;
     float time = 0f;
     bool hasOption = false;
     bool canShot = false;
 
+    private void Start() {
+        options = new GameObject[2];
+    }
 
     private void Update() {
+        optionDefaultPos = new Vector3[2] {
+            new Vector3(transform.position.x + optionPos, optionPos, transform.position.z), new Vector3(transform.position.x + -optionPos, optionPos, transform.position.z)
+            //new Vector3(transform.position.x + optionPos, transform.position.y, transform.position.z), new Vector3(transform.position.x + -optionPos, transform.position.y, transform.position.z)
+        };
+
+
         if (Input.GetKey(KeyCode.Space) && canShot) {
             // 弾をプレイヤーと同じ位置/角度で作成
             Instantiate(bullet, transform.position, transform.rotation);
+            if (hasOption) {
+                for (int i = 0; i < options.Length; i++) {
+                    Instantiate(bullet, options[i].transform.position, options[i].transform.rotation);
+                }
+            }
             canShot = false;
         }
         if (Input.GetKeyDown(KeyCode.X)) {
@@ -24,6 +42,7 @@ public class BulletGenerate : MonoBehaviour {
         }
 
         MoveOption();
+        TrackingOptionToPlayer();
         CanShot();
     }
 
@@ -32,7 +51,6 @@ public class BulletGenerate : MonoBehaviour {
             return;
         }
 
-        options = new GameObject[2];
         for (int i = 0; i < options.Length; i++) {
             options[i] = Instantiate(option);
         }
@@ -41,23 +59,34 @@ public class BulletGenerate : MonoBehaviour {
 
     //生成したオプションを指定位置まで動かす
     private void MoveOption() {
-        if (!hasOption || options[0].transform.position.y > 4) {
+        if (!hasOption || options[0].transform.position.y > optionPos) {
             return;
         }
-        Debug.Log(hasOption);
 
-        Vector3 opr = new Vector3(4, 4, transform.position.z) - transform.position;
+        Vector3 opr = optionDefaultPos[0] - transform.position;
         opr.Normalize();
-        Vector3 opl = new Vector3(-4, 4, transform.position.z) - transform.position;
+        Vector3 opl = optionDefaultPos[1] - transform.position;
         opl.Normalize();
         Vector3[] ov = new Vector3[] { opr, opl };
 
         for (int i = 0; i < options.Length; i++) {
             options[i].transform.position += ov[i] * optionSpeed;
         }
-        Debug.Log(options.Length);
     }
 
+    private void TrackingOptionToPlayer() {
+        if (!hasOption) {
+            return;
+        }
+
+        for (int i = 0; i < options.Length; i++) {
+            Vector3 o2odp = optionDefaultPos[i] - options[i].transform.position;
+            o2odp.Normalize();
+            options[i].transform.position += new Vector3(o2odp.x * optionTrackingSpeed, 0, o2odp.z * optionTrackingSpeed);
+        }
+    }
+
+    //発射準備
     void CanShot() {
         time++;
         //canShot = time >= 60 * shotTime ? true : false;
@@ -65,5 +94,12 @@ public class BulletGenerate : MonoBehaviour {
             canShot = true;
             time = 0f;
         }
+    }
+
+    bool GetOption() {
+        return hasOption;
+    }
+    void SetOption(bool set) {
+        hasOption = set;
     }
 }
