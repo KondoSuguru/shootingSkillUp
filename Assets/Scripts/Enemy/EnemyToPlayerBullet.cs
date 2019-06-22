448b7ecaee4fca27992e8542d1a3d1ebd5ae63e3
KondoSuguru <suguru.pkmn@gmail.com>
2019-06-22 10:17:36 +0900

    Enemyの大きさ変更、Playerに向かう弾を追加

---
+++ Assets/Scripts/Enemy/EnemyToPlayerBullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyToPlayerBullet : MonoBehaviour
+{
+    public float speed = 1.0f;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 vecTarget = target.transform.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(vecTarget);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos += transform.TransformDirection(Vector3.forward) * speed;
+        transform.position = pos;
+    }
+}
