4b2a7071ddb463a92c638aac8d11fe820bfa76ac
TakanoKakeru <llmn.0419@gmail.com>
2019-06-25 18:25:10 +0900

    Revert "Revert "とりあえずコミット、散弾銃を追加""
    This reverts commit 2351f245a2a78d89de7c3147b91e5387678173bc.

---
+++ Assets/Scripts/Player/TripleShot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TripleShot : MonoBehaviour {
+    public Bullet bulletScript;
+    Bullet[] bullets;
+    static bool enabledTriple = false;
+
+    private void Start() {
+        bullets = GetComponentsInChildren<Bullet>();
+    }
+
+    private void Update() {
+        Debug.Log(enabledTriple);
+        if (!enabledTriple) {
+            return;
+        }
+
+        for (int i = 0; i < bullets.Length; i++) {
+            float x = (float)Math.Sin(bullets[i].transform.rotation.y);
+            bullets[i].transform.position += new Vector3(x * bulletScript.GetSpeed(), 0, 0);
+        }
+    }
+
+    public bool GetTriple() {
+        return enabledTriple;
+    }
+    public void SetTriple(bool set) {
+        enabledTriple = set;
+    }
+}
