42fbdf98ceda320bdccc6abbfd41d0c89c1cce4e
KondoSuguru <suguru.pkmn@gmail.com>
2019-06-25 13:39:53 +0900

    EnemyやEmitter周りの修正 EnemyGroupシステム実装

--- Assets/Scripts/Player/PowerUpItemMove.cs
+++ Assets/Scripts/Player/PowerUpItemMove.cs
@@ -15,9 +15,13 @@ public class PowerUpItemMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         Vector3 pos = transform.position;
         pos += new Vector3(0, 0, -speed);
         transform.position = pos;
+
+        if (pos.z > 5)
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,7 +31,7 @@ public class PowerUpItemMove : MonoBehaviour
             Destroy(gameObject);
         }
 
-        if(other.tag == "DeleteArae")
+        if(other.tag == "DeleteArea")
         {
             Destroy(gameObject);
         }
