18da37e89a422e7131d600d269a8abaedb66348e
KondoSuguru <suguru.pkmn@gmail.com>
2019-06-26 10:57:25 +0900

    effectを時間経過で削除する

---
+++ Assets/Scripts/Enemy/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float deleteTime;//消滅する時間(秒)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, deleteTime);
+    }
+}
