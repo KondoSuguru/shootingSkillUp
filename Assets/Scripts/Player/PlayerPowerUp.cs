e391f882c84c81ab67b83cf1edcd1b36c23a6a84
TakanoKakeru <llmn.0419@gmail.com>
2019-07-06 15:24:16 +0900

    ちょっと修正

--- Assets/Scripts/Player/PlayerPowerUp.cs
+++ Assets/Scripts/Player/PlayerPowerUp.cs
@@ -25,8 +25,6 @@ public class PlayerPowerUp : MonoBehaviour {
         audioSource = GetComponent<AudioSource>();
         conditionArray = new bool[maxPoint + 1];
         Initialize();
-
-        razer.SetRazer(true);
     }
     private void Update() {
         PowerUp();
