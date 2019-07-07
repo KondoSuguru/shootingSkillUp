60786d6751dd73719fa239448eea31ad33a8d545
KondoSuguru <suguru.pkmn@gmail.com>
2019-07-07 19:10:46 +0900

    ドラゴンの頭が倒されたら体が順番に消えていくように変更

--- Assets/Scripts/Enemy/Dragon.cs
+++ Assets/Scripts/Enemy/Dragon.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class Dragon : MonoBehaviour
 {
     private int maxCount;
+    private int timer;
+    public GameObject destroyEffect;
 
     // Start is called before the first frame update
     void Start()
     {
         maxCount = transform.childCount;
+        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(transform.childCount == 0)
+        {
+            return;
+        }
+
         if(transform.childCount != maxCount)
         {
-            foreach (Transform n in transform)
+            //foreach (Transform n in transform)
+            //{
+            //    Destroy(n.gameObject);
+            //}
+            timer++;
+            if(timer >= 10)
             {
-                Destroy(n.gameObject);
+                Instantiate(destroyEffect, transform.GetChild(0).position, transform.rotation);
+                Destroy(transform.GetChild(0).gameObject);
+                timer = 0;
             }
         }
     }
