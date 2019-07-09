004908b43185d6b5d8021447ba9bea611bc7ccc9
TakanoKakeru <llmn.0419@gmail.com>
2019-07-09 15:23:17 +0900

    いろいろ

---
+++ Assets/Scripts/Player/DeleteSide.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSide : MonoBehaviour {
+    float time = 0f;
+    float sideDeleteTime = 0.3f;
+
+    void Update() {
+        time += Time.deltaTime;
+        if (time >= sideDeleteTime) {
+            Destroy(gameObject);
+        }
+
+        Debug.Log(time);
+    }
+}
