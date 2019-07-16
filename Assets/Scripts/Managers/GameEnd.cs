5e3e030cf6e85203c374bf33f97bd8f1782b015b
TakanoKakeru <llmn.0419@gmail.com>
2019-07-16 15:24:10 +0900

    終了処理追加

---
+++ Assets/Scripts/Managers/GameEnd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+    }
+}
